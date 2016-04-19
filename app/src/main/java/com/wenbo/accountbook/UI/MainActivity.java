f6b51d96d67769ab718cf0d3cf749593bd021749
Wenbo Ni <Wenbo Ni>
2016-04-19 14:21:08 +0800

    Initial commit

---
+++ app/src/main/java/com/wenbo/accountbook/UI/MainActivity.java
@@ -0,0 +1,17 @@
+package com.wenbo.accountbook.UI;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wenbo.accountbook.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
