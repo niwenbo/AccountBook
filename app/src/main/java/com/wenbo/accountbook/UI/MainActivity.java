5b61c737e4b9961d747e5a0890b32343a2f47b69
wenbo <wenbo_77@163.com>
2016-04-20 10:22:28 +0800

    Rejected commit: Default

--- app/src/main/java/com/wenbo/accountbook/UI/MainActivity.java
+++ app/src/main/java/com/wenbo/accountbook/UI/MainActivity.java
@@ -2,6 +2,7 @@ package com.wenbo.accountbook.UI;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.wenbo.accountbook.R;
 
@@ -11,7 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        //透明状态栏
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        //透明导航栏
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
     }
 }
